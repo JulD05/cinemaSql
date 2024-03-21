9aaff45e97112899d7c3bd0c00d4e7f971bfc961
JulDog <julesdemai@hotmail.fr>
2024-03-21 10:54:51 +0100

    data

--- data.sql
+++ data.sql
@@ -1,91 +1,54 @@
-IF EXISTS (SELECT 1 FROM cinema.Salle)
-BEGIN
-    DELETE FROM cinema.Salle
-END
-INSERT INTO cinema.Salle (Nom, Nb_places) VALUES ("Salle 01", 100), ("Salle 02", 150), ("Salle 03", 200);
+-- DELETE TABLE
+DELETE FROM cinema.Billet;
+DELETE FROM cinema.Plan_Seance;
+DELETE FROM cinema.Creneau;
+DELETE FROM cinema.Film_has_Genre;
+DELETE FROM cinema.Film_has_Acteur;
+DELETE FROM cinema.Film;
+DELETE FROM cinema.Acteur;
+DELETE FROM cinema.Realisateur;
+DELETE FROM cinema.Personne;
+DELETE FROM cinema.Tarif;
+DELETE FROM cinema.LimiteAge;
+DELETE FROM cinema.Genre;
+DELETE FROM cinema.Langue;
+DELETE FROM cinema.Salle;
 
-IF EXISTS (SELECT 1 FROM cinema.Langue)
-BEGIN
-    DELETE FROM cinema.Langue
-END
+
+
+INSERT INTO cinema.Salle (Nom, Nb_places) VALUES ("Salle 01", 100), ("Salle 02", 150), ("Salle 03", 200);
+   
 INSERT INTO cinema.Langue (Langue) VALUES ("VOSTFR"), ("VF");
 
-IF EXISTS (SELECT 1 FROM cinema.LimiteAge)
-BEGIN
-    DELETE FROM cinema.LimiteAge
-END
-INSERT INTO cinema.LimiteAge (Intitule, Age_Mini) VALUES ("Tout public", 0), ("Interdit aux moins de 12 ans", 12), ("Interdit aux moins de 16 ans", 16), ("Interdit aux mineurs", 18);
-
-IF EXISTS (SELECT 1 FROM cinema.Genre)
-BEGIN
-    DELETE FROM cinema.Genre
-END
 INSERT INTO cinema.Genre (Intitule) VALUES ("Drame"), ("Comedie"), ("Aventure"), ("Animation"), ("Famille"), ("Romance"), ("Fantastique"), ("Science-fiction"), ("Comédie"), ("Dramatique"), ("Western"), ("Action"), ("Documentaire"), ("Policier"), ("Fantasy"), ("Thriller");
 
-IF EXISTS (SELECT 1 FROM cinema.Tarif)
-BEGIN
-    DELETE FROM cinema.Tarif
-END
+INSERT INTO cinema.LimiteAge (Intitule, Age_Mini) VALUES ("Tout public", 0), ("Interdit aux moins de 12 ans", 12), ("Interdit aux moins de 16 ans", 16), ("Interdit aux mineurs", 18);
+
 INSERT INTO cinema.Tarif (Intitule, Prix) VALUES ("Tarif plein", 9.80), ("Tarif etudiant", 5), ("Tarif demandeur d'emploi", 5), ("Tarif -14 ans", 3.80);
 
-IF EXISTS (SELECT 1 FROM cinema.Personne)
-BEGIN
-    DELETE FROM cinema.Personne
-END
 INSERT INTO cinema.Personne (Nom, Prenom) VALUES ("Jackson", "Peter"), ("Plante", "Pascal"), ("Triet", "Justine"), ("Wood", "Elijah"), ("McCabe-Lokos", "Maxwell"), ("Machado Graner", "Milo"), ("McKellen", "Ian"), ("Gariépy", "Juliette "), ("Arlaud", "Swann"), ("Mortensen", "Viggo");
 
-IF EXISTS (SELECT 1 FROM cinema.Creneau)
-BEGIN
-    DELETE FROM cinema.Creneau
-END
-INSERT INTO cinema.Creneau (Jour, Heure) VALUES ("2024/03/05", "10:00:00"), ("2024/03/05", "14:00:00"), ("2024/03/05", "18:15:00"), ("2024/03/05", "20:00:00"), ("2024/03/05", "22:00:00");
-
-IF EXISTS (SELECT 1 FROM cinema.Realisateur)
-BEGIN
-    DELETE FROM cinema.Realisateur
-END
 INSERT INTO cinema.Realisateur (Personne_id) VALUES (1), (2), (3);
 
-IF EXISTS (SELECT 1 FROM cinema.Acteur)
-BEGIN
-    DELETE FROM cinema.Acteur
-END
 INSERT INTO cinema.Acteur (Personne_id) VALUES (4), (5), (6), (7), (8), (9), (10);
 
-IF EXISTS (SELECT 1 FROM cinema.Film_has_Genre)
-BEGIN
-    DELETE FROM cinema.Film_has_Genre
-END
-INSERT INTO cinema.Film_has_Genre (Film_id, Genre_id) VALUES ();
+INSERT INTO cinema.Film (Titre, Duree, Date_de_sortie, Synopsis, Avant_premiere, duree_exploitation, Realisateur_id, LimiteAge_id)
+VALUES ("Le Seigneur des Anneaux : La communauté de l'anneau", 178, "2001-12-19", "Dans ce chapitre de la trilogie, le jeune et timide Hobbit, Frodon Sacquet, hérite d'un anneau. Bien loin d'être une simple babiole, il s'agit de l'Anneau Unique, un instrument de pouvoir absolu qui permettrait à Sauron, le Seigneur des ténèbres, de régner sur la Terre du Milieu et de réduire en esclavage ses peuples. Etc.", 0, 8, 1, 1),
+        ("Le Seigneur des Anneaux : Les deux tours", 179, "2002-12-18", "Après la mort de Boromir et la disparition de Gandalf,  la Communauté s'est scindée en trois. Perdus dans les collines d'Emyn Muil  Frodon et Sam découvrent qu'ils sont suivis par Gollum  une créature versatile corrompue par l'Anneau etc.", 0, 8, 1, 1),
+        ("Le Seigneur des Anneaux : Le retour du roi", 201, "2003-12-17", "Les armées de Sauron ont attaqué Minas Tirith  la capitale de Gondor. Jamais ce royaume autrefois puissant n'a eu autant besoin de son roi. Mais Aragorn trouvera-t-il en lui la volonté d'accomplir sa destinée ?", 0, 8, 1, 1),
+        ("Les chambres rouges", 118, "2024-01-10", "Deux jeunes femmes se réveillent chaque matin aux portes du palais de justice pour pouvoir assister au procès hypermédiatisé", 0, 8, 2, 3),
+        ("Anatomie d'une chute", 150, "2023-01-01", "Sandra, Samuel et leur fils malvoyant de 11 ans, Daniel, vivent depuis un an loin de tout, à la montagne. Un jour, Samuel est etc.", 0, 8, 3, 1)
 
-IF EXISTS (SELECT 1 FROM cinema.Plan_Seance)
-BEGIN
-    DELETE FROM cinema.Plan_Seance
-END
-INSERT INTO cinema.Plan_Seance (Jour, Heure, Film_id, Salle_id) VALUES ();
+INSERT INTO cinema.Film_has_Acteur (Film_id, Acteur_id) VALUES (1, 4), (1, 7), (1, 10), (2, 4), (2, 7), (2, 10), (3, 4), (3, 7), (3, 10), (4, 5), (4, 8), (5, 6), (5, 9);
 
-IF EXISTS (SELECT 1 FROM cinema.Film_has_Acteur)
-BEGIN
-    DELETE FROM cinema.Film_has_Acteur
-END
-INSERT INTO cinema.Film_has_Acteur (Film_id, Acteur_id) VALUES ();
+INSERT INTO cinema.Film_has_Genre (Film_id, Genre_id) VALUES (1, 15), (1, 5), (2, 15), (2, 5), (3, 5), (3, 15), (4, 16), (5, 1), (5, 14), (5, 16);
 
-IF EXISTS (SELECT 1 FROM cinema.Billet)
-BEGIN
-    DELETE FROM cinema.Billet
-END
-INSERT INTO cinema.Billet (Tarif_id, Seance_id, Salle_id) VALUES ();
+INSERT INTO cinema.Creneau (Date, Heure) VALUES ("2024-03-05", "10:00:00"), ("2024-03-05", "14:00:00"), ("2024-03-05", "18:15:00"), ("2024-03-05", "20:00:00"), ("2024-03-05", "22:00:00"), ("2024-03-10", "10:00:00"), ("2024-03-10", "14:00:00"), ("2024-03-10", "18:15:00"), ("2024-03-10", "20:00:00"), ("2024-03-10", "22:00:00");
 
-IF EXISTS (SELECT 1 FROM cinema.Film)
-BEGIN
-    DELETE FROM cinema.Film
-END
-INSERT INTO cinema.Film (Titre, Duree, Date_de_sortie, Synopsis, Commentaire_additionnel, Avant_premiere, duree_exploitation, Version_id, Realisateur_id, LimiteAge_id, Tarif_id)
-VALUES ("Le Seigneur des Anneaux : La communauté de l'anneau", 178, "", "Dans ce chapitre de la trilogie, le jeune et timide Hobbit, Frodon Sacquet, hérite d'un anneau. Bien loin d'être une simple babiole, il s'agit de l'Anneau Unique, un instrument de pouvoir absolu qui permettrait à Sauron, le Seigneur des ténèbres, de régner sur la Terre du Milieu et de réduire en esclavage ses peuples. Etc."),
-        ("Le Seigneur des Anneaux : Les deux tours", 179, "", "Après la mort de Boromir et la disparition de Gandalf,  la Communauté s'est scindée en trois. Perdus dans les collines d'Emyn Muil  Frodon et Sam découvrent qu'ils sont suivis par Gollum  une créature versatile corrompue par l'Anneau etc."),
-        ("Le Seigneur des Anneaux : Le retour du roi", 201, "", "Les armées de Sauron ont attaqué Minas Tirith  la capitale de Gondor. Jamais ce royaume autrefois puissant n'a eu autant besoin de son roi. Mais Aragorn trouvera-t-il en lui la volonté d'accomplir sa destinée ?"),
-        ("Les chambres rouges", 118, "", "Deux jeunes femmes se réveillent chaque matin aux portes du palais de justice pour pouvoir assister au procès hypermédiatisé"),
-        ("Anatomie d'une chute", 150, "", "Sandra, Samuel et leur fils malvoyant de 11 ans, Daniel, vivent depuis un an loin de tout, à la montagne. Un jour, Samuel est etc.")
+INSERT INTO cinema.Plan_Seance (Creneau_id, Film_id, Salle_id, Langue_id) VALUES (1, 1, 1, 1), (2, 2, 1, 2), (4, 3, 1, 2), (1, 4, 2, 1), (2, 5, 2, 2), (4, 1, 2, 2), (1, 2, 3, 1), (3, 3, 3, 2), (5, 4, 3, 2), (1, 5, 1, 1), (2, 1, 1, 2), (4, 2, 1, 2), (5, 3, 1, 1), (1, 4, 2, 1), (2, 5, 2, 2), (4, 1, 2, 2), (5, 2, 2, 2), (3, 3, 3, 2), (4, 4, 3, 2), (5, 5, 3, 2);
+
+INSERT INTO cinema.Billet (Tarif_id, Seance_id, Salle_id) VALUES (1, 4, 2), (1, 7, 3), (3, 10, 1), (4, 14, 2), (5, 20, 3);
+
 
 
 
--- schema.sql
+++ schema.sql
@@ -1,249 +1,3 @@
--- Create Schema
-CREATE DATABASE cinema;
-
--- Create Table Salle
-DROP TABLE IF EXISTS Salle;
-CREATE TABLE Salle 
-(
-  id INT PRIMARY KEY NOT NULL,
-  Nom VARCHAR(45) NOT NULL,
-  Nb_Places INT NOT NULL
-);
-
--- Create Table Langue
-DROP TABLE IF EXISTS Langue;
-CREATE TABLE Langue 
-(
-  id INT PRIMARY KEY NOT NULL,
-  Langue VARCHAR(45) NOT NULL
-);
-
--- Create Table Personne
-DROP TABLE IF EXISTS Personne;
-CREATE TABLE Personne 
-(
-  id INT PRIMARY KEY NOT NULL,
-  Nom VARCHAR(45) NULL,
-  Prenom VARCHAR(45) NULL
-);
-
--- Create Table Realisateur
-DROP TABLE IF EXISTS Realisateur;
-CREATE TABLE Realisateur 
-(
-  id INT PRIMARY KEY NOT NULL,
-  Personne_id INT NOT NULL,
-  FOREIGN KEY (Personne_id) REFERENCES Personne(id)
-);
-
--- Create table LimiteAge
-DROP TABLE IF EXISTS LimiteAge;
-CREATE TABLE LimiteAge 
-(
-  id INT PRIMARY KEY NOT NULL,
-  Intitule VARCHAR(45) NOT NULL,
-  Age_Mini INT NOT NULL
-);
-
--- Create Table Tarif
-DROP TABLE IF EXISTS Tarif
-CREATE TABLE Tarif 
-(
-  id INT PRIMARY KEY NOT NULL,
-  Intitule VARCHAR(45) NOT NULL,
-  Prix FLOAT NOT NULL
-);
-
--- Create Table Film
-DROP TABLE IF EXISTS Film;
-CREATE TABLE Film 
-(
-  id INT PRIMARY KEY NOT NULL,
-  Titre VARCHAR(100) NOT NULL,
-  Duree VARCHAR(45) NOT NULL,
-  Date_de_sortie DATE NOT NULL,
-  Synopsis LONGTEXT NULL,
-  Commentaire_additionnel LONGTEXT NULL,
-  Avant_première TINYINT NULL,
-  duree_exploitation DATE NOT NULL,
-  Version_id INT NOT NULL,
-  Realisateur_id INT NOT NULL,
-  LimiteAge_id INT NOT NULL,
-  Tarif_id INT NOT NULL,
-  INDEX fk_Film_Realisateur1_idx (Realisateur_id ASC),
-  INDEX fk_Film_LimiteAge1_idx (LimiteAge_id ASC),
-  INDEX fk_Film_Tarif1_idx (Tarif_id ASC),
-  CONSTRAINT fk_Film_Realisateur1
-    FOREIGN KEY (Realisateur_id)
-    REFERENCES mydb.Realisateur (id)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT fk_Film_LimiteAge1
-    FOREIGN KEY (LimiteAge_id)
-    REFERENCES mydb.LimiteAge (id)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT fk_Film_Tarif1
-    FOREIGN KEY (Tarif_id)
-    REFERENCES mydb.Tarif (id)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-);
-
--- Create table Date
-DROP TABLE IF EXISTS Date;
-CREATE TABLE Date
-(
-  id INT PRIMARY KEY NOT NULL,
-  Date DATETIME NULL,
-  Heure TIME NULL
-);
-
--- Create Table Plan_Seance
-DROP TABLE IF EXISTS Plan_Seance;
-CREATE TABLE Plan_Seance
-(
-  id INT PRIMARY KEY NOT NULL,
-  Salle_id INT NOT NULL,
-  Film_id INT NOT NULL,
-  Date_id INT NOT NULL,
-  Langue_id INT NOT NULL,
-  INDEX fk_Seance_Salle1_idx (Salle_id ASC),
-  INDEX fk_Plan_Seance_Film1_idx (Film_id ASC),
-  INDEX fk_Plan_Seance_Date1_idx (Date_id ASC),
-  INDEX fk_Plan_Seance_Langue1_idx (Langue_id ASC),
-  CONSTRAINT fk_Seance_Salle1
-    FOREIGN KEY (Salle_id)
-    REFERENCES mydb.Salle (id)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT fk_Plan_Seance_Film1
-    FOREIGN KEY (Film_id)
-    REFERENCES mydb.Film (id)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT fk_Plan_Seance_Date1
-    FOREIGN KEY (Date_id)
-    REFERENCES mydb.Date (id)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT fk_Plan_Seance_Langue1
-    FOREIGN KEY (Langue_id)
-    REFERENCES mydb.Langue (id)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-);
-
--- Create Table Acteur
-DROP TABLE IF EXISTS Acteur;
-CREATE TABLE Acteur
-(
-  id INT NOT NULL,
-  Personne_id INT NOT NULL,
-  PRIMARY KEY (id, Personne_id),
-  INDEX fk_Acteur_Personne1_idx (Personne_id ASC),
-  CONSTRAINT fk_Acteur_Personne1
-    FOREIGN KEY (Personne_id)
-    REFERENCES mydb.Personne (id)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-);
-
--- Create Table Genre
-DROP TABLE IF EXISTS Genre;
-CREATE TABLE Genre
-(
-  id INT PRIMARY KEY NOT NULL,
-  Intitule VARCHAR(45) NOT NULL
-);
-
--- Create Table Film_has_Genre
-DROP TABLE IF EXISTS Film_has_Genre;
-CREATE TABLE Film_has_Genre (
-  Film_id INT NOT NULL,
-  Genre_id INT NOT NULL,
-  PRIMARY KEY (Film_id, Genre_id),
-  INDEX fk_Film_has_Genre_Genre1_idx (Genre_id ASC),
-  INDEX fk_Film_has_Genre_Film1_idx (Film_id ASC),
-  CONSTRAINT fk_Film_has_Genre_Film1
-    FOREIGN KEY (Film_id)
-    REFERENCES mydb.Film (id)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT fk_Film_has_Genre_Genre1
-    FOREIGN KEY (Genre_id)
-    REFERENCES mydb.Genre (id)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-);
-
-DROP TABLE IF EXISTS Billet;
-CREATE TABLE Billet
-(
-  id INT NOT NULL,
-  Tarif_id INT NOT NULL,
-  Seance_id INT NOT NULL,
-  Salle_id INT NOT NULL,
-  PRIMARY KEY (id, Tarif_id, Seance_id, Salle_id),
-  INDEX fk_Billet_Tarif1_idx (Tarif_id ASC),
-  INDEX fk_Billet_Seance1_idx (Seance_id ASC),
-  INDEX fk_Billet_Salle1_idx (Salle_id ASC),
-  CONSTRAINT fk_Billet_Tarif1
-    FOREIGN KEY (Tarif_id)
-    REFERENCES mydb.Tarif (id)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT fk_Billet_Seance1
-    FOREIGN KEY (Seance_id)
-    REFERENCES mydb.Plan_Seance (id)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT fk_Billet_Salle1
-    FOREIGN KEY (Salle_id)
-    REFERENCES mydb.Salle (id)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-);
-
--- Create table Film_has_Acteur
-DROP TABLE IF EXISTS Film_has_Acteur;
-CREATE TABLE Film_has_Acteur
-(
-  Film_id INT NOT NULL,
-  Acteur_id INT NOT NULL,
-  PRIMARY KEY (Film_id, Acteur_id),
-  INDEX fk_Film_has_Acteur_Acteur1_idx (Acteur_id ASC),
-  INDEX fk_Film_has_Acteur_Film1_idx (Film_id ASC),
-  CONSTRAINT fk_Film_has_Acteur_Film1
-    FOREIGN KEY (Film_id)
-    REFERENCES mydb.Film (id)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT fk_Film_has_Acteur_Acteur1
-    FOREIGN KEY (Acteur_id)
-    REFERENCES mydb.Acteur (id)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--------------- A UTILISER POUR PHPMYADMIN--------------
-
-
-
 DROP DATABASE IF EXISTS cinema;
 -- Create Schema
 CREATE DATABASE cinema;
@@ -312,14 +66,11 @@ CREATE TABLE cinema.Film
   Synopsis LONGTEXT NULL,
   Commentaire_additionnel LONGTEXT NULL,
   Avant_premiere TINYINT NULL,
-  duree_exploitation DATE NOT NULL,
-  Version_id INT NOT NULL,
+  duree_exploitation INT NOT NULL,
   Realisateur_id INT NOT NULL,
   LimiteAge_id INT NOT NULL,
-  Tarif_id INT NOT NULL,
   INDEX fk_Film_Realisateur1_idx (Realisateur_id ASC),
   INDEX fk_Film_LimiteAge1_idx (LimiteAge_id ASC),
-  INDEX fk_Film_Tarif1_idx (Tarif_id ASC),
   CONSTRAINT fk_Film_Realisateur1
     FOREIGN KEY (Realisateur_id)
     REFERENCES cinema.Realisateur (id)
@@ -329,20 +80,15 @@ CREATE TABLE cinema.Film
     FOREIGN KEY (LimiteAge_id)
     REFERENCES cinema.LimiteAge (id)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT fk_Film_Tarif1
-    FOREIGN KEY (Tarif_id)
-    REFERENCES cinema.Tarif (id)
-    ON DELETE NO ACTION
     ON UPDATE NO ACTION
 );
 
--- Create table Date
+-- Create table Creneau
 DROP TABLE IF EXISTS cinema.Creneau;
 CREATE TABLE cinema.Creneau
 (
   id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  Jour DATE NOT NULL,
+  Date DATE NOT NULL,
   Heure TIME NOT NULL
 );
 
@@ -353,11 +99,11 @@ CREATE TABLE cinema.Plan_Seance
   id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
   Salle_id INT NOT NULL,
   Film_id INT NOT NULL,
-  Date_id INT NOT NULL,
+  Creneau_id INT NOT NULL,
   Langue_id INT NOT NULL,
   INDEX fk_Seance_Salle1_idx (Salle_id ASC),
   INDEX fk_Plan_Seance_Film1_idx (Film_id ASC),
-  INDEX fk_Plan_Seance_Date1_idx (Date_id ASC),
+  INDEX fk_Plan_Seance_Creneau1_idx (Creneau_id ASC),
   INDEX fk_Plan_Seance_Langue1_idx (Langue_id ASC),
   CONSTRAINT fk_Seance_Salle1
     FOREIGN KEY (Salle_id)
@@ -369,8 +115,8 @@ CREATE TABLE cinema.Plan_Seance
     REFERENCES cinema.Film (id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT fk_Plan_Seance_Date1
-    FOREIGN KEY (Date_id)
+  CONSTRAINT fk_Plan_Seance_Creneau1
+    FOREIGN KEY (Creneau_id)
     REFERENCES cinema.Creneau (id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
